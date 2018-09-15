91b287f8f741975fe67c699b88cb62a442da8546
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-09-15 20:56:08 +0200

    Added some helper functions

--- helper_functions/helper_functions.py
+++ helper_functions/helper_functions.py
@@ -6,17 +6,32 @@ def next_hour(current_date):
     return current_date + timedelta(hours=1)
 
 
-def print_datetime_slot_info(date_time):
+def get_datetime_slot_info(date_time):
+    """
+
+    :param date_time:
+    :return:
+    """
     datetime(2009, 10, 5, 18, 00)
     day_name = date_time.strftime("%A")
     date = date_time.date()
     start_time = date_time.strftime('%H:%M:%S')
-    print("{} {} from {}.".format(day_name, date, start_time))
+    return "{} {} from {}.".format(day_name, date, start_time)
 
 
 def string_to_datetime(date_time_string):
+    """
+
+    :param date_time_string:
+    :return:
+    """
     return parse(date_time_string)
 
 
 def datetime_to_string(date_time_string):
+    """
+
+    :param date_time_string:
+    :return:
+    """
     return date_time_string.strftime("%Y-%m-%d %H:%M:%S")
