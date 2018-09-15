d93a20d5491b57da7e4799c022b4b287e95b670d
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-09-15 18:21:10 +0200

    Added the Json serializer

--- interviews_calendar/calendar.py
+++ interviews_calendar/calendar.py
@@ -1,5 +1,6 @@
 from datetime import (datetime)
-from helper_functions.helper_functions import next_hour
+
+from helper_functions.helper_functions import (next_hour, string_to_datetime, datetime_to_string)
 
 
 class CalendarSlot:
@@ -12,11 +13,26 @@ class CalendarSlot:
         self.start_time = start_time
         self.end_time = next_hour(start_time)
 
+    def serialize(self):
+        response = dict()
+        response["interviewers"] = self.interviewers
+        response["interviewee"] = self.interviewee
+        response["start_time"] = datetime_to_string(self.start_time)
+        response["end_time"] = datetime_to_string(self.end_time)
+
+        return response
+
     def is_available(self):
         """
         :return: Boolean value; if the interviewee name is "", this means that this slot was not allocated yet
         """
         return not self.interviewee
+
+    def set_interviewee(self, interviewee: str):
+        """
+        :return: Boolean value; if the interviewee name is "", this means that this slot was not allocated yet
+        """
+        self.interviewee = interviewee
 
 
 class Calendar:
@@ -34,18 +50,27 @@ class Calendar:
                                 start_time=start_time,
                                 interviewee=interviewee)
         self.slots.append(new_slot)
-        return new_slot
+        return new_slot.serialize()
 
     def add_slots(self, interviewers: list(), start_times: list(), interviewee: str = ""):
+        added_slots = []
         for time in start_times:
-            self.add_slot(interviewers=interviewers,
-                          start_time=time,
-                          interviewee=interviewee)
+            time = string_to_datetime(time)
+            new_slot = self.add_slot(interviewers=interviewers,
+                                     start_time=time,
+                                     interviewee=interviewee)
+            added_slots.append(new_slot)
+
+        return added_slots
 
     def get_available_slots(self):
         available_slots = list()
-        for slot in self.slots:
-            if slot.is_available():
-                available_slots.append(slot)
+        for slot_index in range(len(self.slots)):
+            if self.slots[slot_index].is_available():
+                available_slots.append(self.slots[slot_index].serialize())
+        print(available_slots)
         return available_slots
 
+    def set_interview(self, slot_id: int, interviewee: str):
+        self.slots[slot_id].set_interviewee(interviewee)
+        return self.slots[slot_id].serialize()
