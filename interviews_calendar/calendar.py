c2c0eb2d13e35aca1da0c0991fb2d4bd8af61547
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-09-15 22:47:55 +0200

    Documented the calendar classes

--- interviews_calendar/calendar.py
+++ interviews_calendar/calendar.py
@@ -16,9 +16,9 @@ class CalendarSlot:
     def __init__(self, interviewers: list(), start_time: datetime, interviewee: str = ""):
         """
 
-        :param interviewers:
-        :param start_time:
-        :param interviewee:
+        :param interviewers: list of the names of the interviewers
+        :param start_time: datetime object represents the start period at the calender
+        :param interviewee: string represents the interviewee name (optional)
         """
         self.interviewers = interviewers
         self.interviewee = interviewee
@@ -27,8 +27,8 @@ class CalendarSlot:
 
     def serialize(self):
         """
-
-        :return:
+        convert the class to serializable object
+        :return: dictionary contains the class info in as a json serializable object
         """
         response = dict()
         response["interviewers"] = self.interviewers
@@ -40,24 +40,24 @@ class CalendarSlot:
 
     def is_available(self):
         """
-
+        check if the current slot is available for an interviewee or not
         :return: Boolean value; if the interviewee name is "", this means that this slot was not allocated yet
         """
         return not self.interviewee
 
     def set_interviewee(self, interviewee: str):
         """
-
-        :return: Boolean value; if the interviewee name is "", this means that this slot was not allocated yet
+        assign the current slot to a specific interviewee
+        :param interviewee: string represents the interviewee name
         """
         self.interviewee = interviewee
 
     def in_time_range(self, start_time: datetime, end_time: datetime):
         """
-
-        :param start_time:
-        :param end_time:
-        :return:
+        checks if the current slot is within the given time range or not
+        :param start_time: start period of the given range
+        :param end_time: end period of the given range
+        :return: Boolean value; represents if the current slot is within the current range or not
         """
         return end_time >= self.start_time >= start_time
 
@@ -68,14 +68,14 @@ class Calendar:
     """
     def __init__(self):
         """
-
+        initialize the assigned slots so far with an empty array
         """
         self.slots = list()
 
     def get_slots(self):
         """
-
-        :return:
+        get all the slots in the calendar so far
+        :return: slots in the calendar
         """
         slots = []
         for slot in self.slots:
@@ -84,11 +84,11 @@ class Calendar:
 
     def add_slot(self, interviewers: list(), start_time: datetime, interviewee: str = ""):
         """
-
-        :param interviewers:
-        :param start_time:
-        :param interviewee:
-        :return:
+        add a slot to the calendar
+        :param interviewers: names of the interviewers
+        :param start_time: time in datetime format
+        :param interviewee: interviewee name if available
+        :return: the recently added slot
         """
         new_slot = CalendarSlot(interviewers=interviewers,
                                 start_time=start_time,
@@ -98,11 +98,11 @@ class Calendar:
 
     def add_slots(self, interviewers: list(), start_times: list(), interviewee: str = ""):
         """
-
-        :param interviewers:
-        :param start_times:
-        :param interviewee:
-        :return:
+        add slots to the calendar
+        :param interviewers: names of interviewers
+        :param start_times: list of start datetimes
+        :param interviewee: name of the interviewee
+        :return: list of recently added slots
         """
         added_slots = []
         for time in start_times:
@@ -116,8 +116,8 @@ class Calendar:
 
     def get_available_slots(self):
         """
-
-        :return:
+        get the allocated slots which don't have appointments with interviewees yet, so the interviewee can choose one of them!
+        :return: slots which are not allocated so far
         """
         available_slots = list()
         for slot_index in range(len(self.slots)):
@@ -127,20 +127,21 @@ class Calendar:
 
     def set_interview(self, slot_id: int, interviewee: str):
         """
-
-        :param slot_id:
-        :param interviewee:
-        :return:
+        Pair the interviewer with the interviewee, based on the slot which the interviewee will choose
+        :param slot_id: array index of the available slots (it should be replaced with real ID)
+        :param interviewee: interviewee name
+        :return: chosen slot
         """
+        # TODO: instead of using the self.slots, we must use the unassigned slots yet (current logic is wrong).
         self.slots[slot_id].set_interviewee(interviewee)
         return self.slots[slot_id].serialize()
 
     def search(self, start_date: str, end_date: str):
         """
-
-        :param start_date:
-        :param end_date:
-        :return:
+        get the slots that were added to the calendar within a specific time range
+        :param start_date: start date in the search query
+        :param end_date: end date in the search query
+        :return: valid search results
         """
         valid_slots = []
         start_date = string_to_datetime(start_date)
