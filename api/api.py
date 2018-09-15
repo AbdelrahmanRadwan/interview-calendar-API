c8288eec1ba8669f623636094103c867f2661df1
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-09-15 20:55:56 +0200

    Added code comments

--- api/api.py
+++ api/api.py
@@ -1,32 +1,44 @@
-from pathlib import Path
-
-from interviews_calendar.calendar import Calendar
+import sys
+import os
 
 from flask import Flask, jsonify, request
 
-ROOT_DIR = str(Path(__file__).parents[2])
-app = Flask(__name__)
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
+from interviews_calendar.calendar import Calendar
+
+app = Flask(__name__)
 interviews_calendar = Calendar()
 
 
 @app.route('/interviews_calendar/heartbeat', methods=['GET'])
 def heartbeat():
+    """
+    A dummy route to check if the endpoints is up or not!
+    :return:
+    """
     return jsonify({"who": 'Hello <3'})
 
 
 @app.route('/interviews_calendar/admin/view-all', methods=['GET'])
 def view_calendar():
+    """
+
+    :return:
+    """
     response = interviews_calendar.get_slots()
     return jsonify(response)
 
 
 @app.route('/interviews_calendar/interviewee/available-times', methods=['GET', 'POST'])
 def available_times():
+    """
+
+    :return:
+    """
     req = request.get_json()
     if request.method == 'GET':
         response = interviews_calendar.get_available_slots()
-        print(response)
         return jsonify(response)
 
     elif request.method == 'POST':
@@ -38,6 +50,10 @@ def available_times():
 
 @app.route('/interviews_calendar/interviewer/add-slots', methods=['POST'])
 def add_available_times():
+    """
+
+    :return:
+    """
     req = request.get_json()
     interviewers = req["interviewers"]
     interviewee = req.get("interviewee") or ""
@@ -53,15 +69,17 @@ def add_available_times():
 
 @app.route('/interviews_calendar/search', methods=['GET'])
 def search():
+    """
+
+    :return:
+    """
     req = request.get_json()
     start_date = req["start_date"]
     end_date = req["end_date"]
 
     response = interviews_calendar.search(start_date=start_date,
                                           end_date=end_date)
-    print(response)
     return jsonify(response)
 
 
-
-app.run(debug=False, port=8080, host='0.0.0.0')
+app.run(debug=False, port=8080, host='0.0.0.0')  # Running on http://0.0.0.0:8080/
